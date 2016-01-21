aab0256be72bcee9b6cd9d39b64afe3d19aff942
Alexander Knowles <alexander.knowles@gmail.com>
2016-01-20 21:45:03 -0600

    Implement test for too few arguments.
    Following example at:
    http://www.developer.com/tech/article.php/3669436/Test-Driving-a-Java-Command-Line-Application.htm

--- src/test/java/com/skraylabs/poker/ApplicationTest.java
+++ src/test/java/com/skraylabs/poker/ApplicationTest.java
@@ -1,10 +1,15 @@
 package com.skraylabs.poker;
 
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class ApplicationTest {
 
@@ -15,8 +20,23 @@ public class ApplicationTest {
   public void tearDown() throws Exception {}
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testShowUsageForTooFewArguments() {
+    // Set up
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    PrintStream console = System.out;
+    try {
+      System.setOut(new PrintStream(output));
+      String[] args = {""};
+      // Exercise
+      Application.main(args);
+    } finally {
+      // Tear down
+      System.setOut(console);
+    }
+    // Verify
+    String outputString = output.toString();
+    assertThat(outputString, allOf(containsString("Too few arguments"),
+        containsString("Usage: PokerCalculator filepath")));
   }
 
 }
