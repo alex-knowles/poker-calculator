822152742d578a989f373ce16c310c56f9bfb4f2
Alexander Knowles <alexander.knowles@gmail.com>
2016-01-20 22:42:12 -0600

    Re-factor some commonality into Application.execute().
    Also add some method javadoc.

--- src/main/java/com/skraylabs/poker/Application.java
+++ src/main/java/com/skraylabs/poker/Application.java
@@ -8,23 +8,44 @@ public class Application {
 
   private static Application app;
 
+  private String errorMessage;
+
   public static void main(String... args) {
     app = new Application();
     app.execute(args);
   }
 
+  /**
+   * Execute application
+   *
+   * @param args should be exactly 1 string specifying the input filepath to read from.
+   */
   public void execute(String... args) {
-    validate(args);
+    if (!validate(args)) {
+      System.out.println(errorMessage);
+      System.out.println(MSG_USAGE);
+    } else {
+      // TODO: process input from file
+    }
   }
 
-  private void validate(String[] args) {
+  /**
+   * Validate the command line arguments. Will set errorMessage to an appropriate value if
+   * validation fails.
+   *
+   * @param args command line arguments
+   * @return true if valid, false otherwise
+   */
+  private boolean validate(String[] args) {
+    boolean result = true;
     if (args.length < 1) {
-      System.out.println(MSG_TOO_FEW_ARGS);
-      System.out.println(MSG_USAGE);
+      errorMessage = MSG_TOO_FEW_ARGS;
+      result = false;
     } else if (args.length > 1) {
-      System.out.println(MSG_TOO_MANY_ARGS);
-      System.out.println(MSG_USAGE);
+      errorMessage = MSG_TOO_MANY_ARGS;
+      result = false;
     }
+    return result;
   }
 
 }
