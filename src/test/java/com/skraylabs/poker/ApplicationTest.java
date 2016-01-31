9dd8fc419ea3dcf3f22763d0049a7584747e263a
Alexander Knowles <alexander.knowles@gmail.com>
2016-01-31 03:01:54 -0600

    Implement test for valid arguments.

--- src/test/java/com/skraylabs/poker/ApplicationTest.java
+++ src/test/java/com/skraylabs/poker/ApplicationTest.java
@@ -115,6 +115,34 @@ public class ApplicationTest {
   }
 
   @Test
+  public void testValidInput() {
+    // Set up
+    final String input = "5h 7d Ts Kc 2d%n"
+        + "5d 5s%n";
+    app = new SafeExitApplication() {
+      @Override
+      InputStream createInputStream() {
+        return new ByteArrayInputStream(input.getBytes());
+      }
+    };
+
+    // Exercise
+    app.execute("foo.txt");
+    // Verify
+    assertThat(this.errorCode, equalTo(0));
+    String output = outputStream.toString();
+    assertThat(output, containsString("Royal Flush: 0%"));
+    assertThat(output, containsString("Straight Flush: 0%"));
+    assertThat(output, containsString("Four of a Kind: 0%"));
+    assertThat(output, containsString("Full House: 0%"));
+    assertThat(output, containsString("Flush: 0%"));
+    assertThat(output, containsString("Straight: 0%"));
+    assertThat(output, containsString("Three of a Kind: 100%"));
+    assertThat(output, containsString("Two Pair: 0%"));
+    assertThat(output, containsString("Two of a Kind: 100%"));
+  }
+
+  @Test
   public void testAbortInputFileNotOpened() {
     // Exercise
     final String filepath = "absent_file.txt";
