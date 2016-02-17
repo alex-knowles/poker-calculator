262c862f8454f62dcc1af4c4f3fbbe84cdae72bf
Alexander Knowles <alexander.knowles@gmail.com>
2016-02-17 08:18:03 -0600

    Define Rank and Suit enumerations.

---
+++ src/main/java/com/skraylabs/poker/model/Rank.java
@@ -0,0 +1,20 @@
+package com.skraylabs.poker.model;
+
+/**
+ * Ranks of playing cards.
+ */
+public enum Rank {
+  Ace,
+  King,
+  Queen,
+  Jack,
+  Ten,
+  Nine,
+  Eight,
+  Seven,
+  Six,
+  Five,
+  Four,
+  Three,
+  Two
+}
---
+++ src/main/java/com/skraylabs/poker/model/Suit.java
@@ -0,0 +1,11 @@
+package com.skraylabs.poker.model;
+
+/**
+ * The four suits of playing cards.
+ */
+public enum Suit {
+  Spades,
+  Hearts,
+  Diamonds,
+  Clubs
+}
