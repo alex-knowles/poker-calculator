312028cf73836ec4f5b56e9a904b3532524f8250
Alexander Knowles <alexander.knowles@gmail.com>
2016-02-18 17:01:27 -0600

    Define some constructor tests for InvalidCardFormatException.

---
+++ src/test/java/com/skraylabs/poker/model/InvalidCardFormatExceptionTest.java
@@ -0,0 +1,42 @@
+package com.skraylabs.poker.model;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvalidCardFormatExceptionTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void testDefaultConstructor() {
+    // Exercise
+    InvalidCardFormatException exception = new InvalidCardFormatException();
+    // Verify
+    String message = exception.getMessage();
+    String invalidString = exception.getInvalidString();
+    assertThat(message, equalTo(InvalidCardFormatException.MSG_DEFAULT));
+    assertThat(invalidString, is(nullValue()));
+  }
+
+  @Test
+  public void testInitializingConstructor() {
+    // Exercise
+    InvalidCardFormatException exception = new InvalidCardFormatException("5x");
+    // Verify
+    String message = exception.getMessage();
+    String invalidString = exception.getInvalidString();
+    String expectedMessage = String.format(InvalidCardFormatException.MSG_WITH_SAMPLE, "5x");
+    assertThat(message, equalTo(expectedMessage));
+    assertThat(invalidString, equalTo("5x"));
+  }
+}
