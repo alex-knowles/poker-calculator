b9f80fa7ba5af96b456103ff6f38db6043d6a088
Alexander Knowles <alexander.knowles@gmail.com>
2016-02-18 22:31:43 -0600

    CardFactoryTest: test invalid input: blank string.
    An empty string is blank, but a blank string is
    not necessarily empty. A blank string may contain
    one or many whitespace characters.

--- src/test/java/com/skraylabs/poker/model/CardFactoryTest.java
+++ src/test/java/com/skraylabs/poker/model/CardFactoryTest.java
@@ -53,6 +53,16 @@ public class CardFactoryTest {
     CardFactory.createCardFromString("");
   }
 
+  @Test
+  public void testInvalidInput_blank() throws InvalidCardFormatException {
+    // Verify
+    String expectedMessage = String.format(InvalidCardFormatException.MSG_WITH_INVALID_STRING, " ");
+    exception.expect(InvalidCardFormatException.class);
+    exception.expectMessage(expectedMessage);
+    // Exercise
+    CardFactory.createCardFromString(" ");
+  }
+
   /**
    * Test utility to assert 3 properties of a given {@link Card}.
    * <ul>
