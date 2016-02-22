79390f581fd92b7375d4844f0e5f280d7fe021b6
Alexander Knowles <alexander.knowles@gmail.com>
2016-02-22 17:37:01 -0600

    Board: add constructors.

--- src/main/java/com/skraylabs/poker/model/Board.java
+++ src/main/java/com/skraylabs/poker/model/Board.java
@@ -25,4 +25,51 @@ public class Board {
    * Card dealt in the fourth and final round (the "river").
    */
   public Card riverCard;
+
+  /**
+   * Default constructor.
+   */
+  public Board() {
+    this(null, null, null);
+  }
+
+  /**
+   * Initializing constructor.
+   *
+   * @param flopCard1
+   * @param flopCard2
+   * @param flopCard3
+   */
+  public Board(Card flopCard1, Card flopCard2, Card flopCard3) {
+    this(flopCard1, flopCard2, flopCard3, null);
+  }
+
+  /**
+   * Initializing constructor.
+   *
+   * @param flopCard1
+   * @param flopCard2
+   * @param flopCard3
+   * @param turnCard
+   */
+  public Board(Card flopCard1, Card flopCard2, Card flopCard3, Card turnCard) {
+    this(flopCard1, flopCard2, flopCard3, turnCard, null);
+  }
+
+  /**
+   * Initializing constructor.
+   *
+   * @param flopCard1
+   * @param flopCard2
+   * @param flopCard3
+   * @param turnCard
+   * @param riverCard
+   */
+  public Board(Card flopCard1, Card flopCard2, Card flopCard3, Card turnCard, Card riverCard) {
+    this.flopCard1 = flopCard1;
+    this.flopCard2 = flopCard2;
+    this.flopCard3 = flopCard3;
+    this.turnCard = turnCard;
+    this.riverCard = riverCard;
+  }
 } // end of class Board
