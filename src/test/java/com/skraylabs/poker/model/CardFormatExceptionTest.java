a37c8b8beb95d8464c6d9c531d9e7489bebe2539
Alexander Knowles <alexander.knowles@gmail.com>
2016-03-01 08:39:02 -0600

    Rename test class to CardFormatExceptionTest.

--- src/test/java/com/skraylabs/poker/model/CardFormatExceptionTest.java
+++ src/test/java/com/skraylabs/poker/model/CardFormatExceptionTest.java
@@ -7,7 +7,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class InvalidCardFormatExceptionTest {
+public class CardFormatExceptionTest {
 
   @Before
   public void setUp() throws Exception {}
