6ea2ed4c8ef1ad45671a0c517cab244358a51c91
Alexander Knowles <alexander.knowles@gmail.com>
2016-03-03 08:34:13 -0600

    Initial implementation of GameStateFormatException.

---
+++ src/main/java/com/skraylabs/poker/model/GameStateFormatException.java
@@ -0,0 +1,40 @@
+package com.skraylabs.poker.model;
+
+/**
+ * Checked exception thrown when a string representing a {@link GameState} is formatted incorrectly.
+ * This applies to general syntax errors that don't qualify for a more specific exception (
+ * {@link CardFormatException}, {@link BoardFormatException}, {@link GameStateFormatException}).
+ */
+public class GameStateFormatException extends PokerFormatException {
+
+  /**
+   * Generated serial ID.
+   */
+  private static final long serialVersionUID = -3555305759559275725L;
+
+  /**
+   * Default detail message used when the invalid string sample is not provided.
+   */
+  public static final String MSG_DEFAULT =
+      "The provided string could not be resolved to a GameState format.";
+
+  /**
+   * Detail message used when the invalid string sample is provided. Expects one String argument.
+   */
+  public static final String MSG_WITH_INVALID_STRING =
+      "The string <\"%s\"> could not be resolved to a GameState format.";
+
+  /**
+   * Default constructor.
+   */
+  public GameStateFormatException() {
+  }
+
+  /**
+   * Initializing constructor.
+   *
+   * @param message detail message
+   */
+  public GameStateFormatException(String message) {
+  }
+}
