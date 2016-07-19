b88cc2ccbc8917204f9d9f4760e87aa3cac239a9
Alexander Knowles <alexander.knowles@gmail.com>
2016-07-19 17:25:57 -0500

    Factor-out some logic that determines a Two of a Kind.
    This is a partial re-factoring, the original code
    is still in place, but will be replaced in a future
    commit.

--- src/main/java/com/skraylabs/poker/OddsCalculator.java
+++ src/main/java/com/skraylabs/poker/OddsCalculator.java
@@ -7,6 +7,7 @@ import com.skraylabs.poker.model.GameState;
 import com.skraylabs.poker.model.Pocket;
 import com.skraylabs.poker.model.Rank;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -49,6 +50,29 @@ class OddsCalculator {
       double numberOfOuts = numberOfCardsToBeDealt * cards.size() * 3;
       double numberOfRemainingCards = 52 - cards.size();
       result = numberOfOuts / numberOfRemainingCards;
+    }
+    return result;
+  }
+
+  /**
+   * Helper method that determines if a Two of a Kind exists on a given combination of board and
+   * pocket cards.
+   *
+   * @param board cards collected from a {@link Board}
+   * @param pocket cards collected from a {@link Pocket}
+   * @return {@code true} if there is at least one Two of a Kind; {@code false} otherwise
+   */
+  static boolean hasTwoOfAKind(Collection<Card> board, Collection<Card> pocket) {
+    boolean result = false;
+    Collection<Card> cards = new ArrayList<>(board);
+    cards.addAll(pocket);
+    Map<Rank, Long> countByRank =
+        cards.stream().collect(Collectors.groupingBy(Card::getRank, Collectors.counting()));
+    for (Long count : countByRank.values()) {
+      if (count >= 2) {
+        result = true;
+        break;
+      }
     }
     return result;
   }
