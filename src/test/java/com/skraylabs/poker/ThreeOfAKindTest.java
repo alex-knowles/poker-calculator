2c3497aea3df238c43129a6270bd70e5e0385396
Alexander Knowles <alexander.knowles@gmail.com>
2016-07-21 22:16:57 -0500

    Re-factor test class for three of a kinds.
    Remove dependency on GameStateFactory.
    
    Establish a common test fixture to make test methods
    easier to follow.

--- src/test/java/com/skraylabs/poker/ThreeOfAKindTest.java
+++ src/test/java/com/skraylabs/poker/ThreeOfAKindTest.java
@@ -6,25 +6,78 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import com.skraylabs.poker.model.BoardFormatException;
 import com.skraylabs.poker.model.Card;
 import com.skraylabs.poker.model.CardFormatException;
-import com.skraylabs.poker.model.CardUtils;
-import com.skraylabs.poker.model.GameState;
-import com.skraylabs.poker.model.GameStateFactory;
 import com.skraylabs.poker.model.GameStateFormatException;
 import com.skraylabs.poker.model.PocketFormatException;
+import com.skraylabs.poker.model.Rank;
+import com.skraylabs.poker.model.Suit;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ThreeOfAKindTest {
 
+  /**
+   * Test board.
+   *
+   * <p>
+   * Ah Kh Qh
+   */
+  Collection<Card> boardAceKingQueen;
+
+  /**
+   * Test pocket. No Three of a Kind with {@link #boardAceKingQueen}.
+   *
+   * <p>
+   * 2d 7c
+   */
+  Collection<Card> pocketTwoSeven;
+
+  /**
+   * Test pocket. Makes triple queens with {@link #boardAceKingQueen}.
+   *
+   * <p>
+   * Qd Qc
+   */
+  Collection<Card> pocketQueens;
+
+  /**
+   * Set up test board.
+   */
+  @Before
+  public void setUpBoardAceKingQueen() {
+    boardAceKingQueen = new ArrayList<Card>();
+    boardAceKingQueen.add(new Card(Rank.Ace, Suit.Hearts));
+    boardAceKingQueen.add(new Card(Rank.King, Suit.Hearts));
+    boardAceKingQueen.add(new Card(Rank.Queen, Suit.Hearts));
+  }
+
+  /**
+   * Set up test pocket.
+   */
+  @Before
+  public void setUpPocketTwoSeven() {
+    pocketTwoSeven = new ArrayList<Card>();
+    pocketTwoSeven.add(new Card(Rank.Two, Suit.Diamonds));
+    pocketTwoSeven.add(new Card(Rank.Seven, Suit.Clubs));
+  }
+
+  /**
+   * Set up test pocket.
+   */
+  @Before
+  public void setUpPocketQueens() {
+    pocketQueens = new ArrayList<Card>();
+    pocketQueens.add(new Card(Rank.Queen, Suit.Diamonds));
+    pocketQueens.add(new Card(Rank.Queen, Suit.Clubs));
+  }
+
   @Test
-  public void handWithNoThreeOfAKindReturnsFalse() throws CardFormatException, BoardFormatException,
-      PocketFormatException, GameStateFormatException {
-    GameState state = GameStateFactory.createGameStateFromString("Ah Kh Qh\n" + "2d 7c");
-    Collection<Card> board = CardUtils.collectCards(state.getBoard());
-    Collection<Card> pocket = CardUtils.collectCards(state.getPockets()[0]);
-    Collection<Card> cards = ProbabilityCalculator.collectHandCards(board, pocket);
+  public void handWithNoThreeOfAKindReturnsFalse() {
+    Collection<Card> cards =
+        ProbabilityCalculator.collectHandCards(boardAceKingQueen, pocketTwoSeven);
 
     boolean result = ProbabilityCalculator.hasThreeOfAKind(cards);
 
@@ -34,10 +87,8 @@ public class ThreeOfAKindTest {
   @Test
   public void handWithThreeOfAKindReturnsTrue() throws CardFormatException, BoardFormatException,
       PocketFormatException, GameStateFormatException {
-    GameState state = GameStateFactory.createGameStateFromString("Ah Kh Qh\n" + "Qd Qc");
-    Collection<Card> board = CardUtils.collectCards(state.getBoard());
-    Collection<Card> pocket = CardUtils.collectCards(state.getPockets()[0]);
-    Collection<Card> cards = ProbabilityCalculator.collectHandCards(board, pocket);
+    Collection<Card> cards =
+        ProbabilityCalculator.collectHandCards(boardAceKingQueen, pocketQueens);
 
     boolean result = ProbabilityCalculator.hasThreeOfAKind(cards);
 
